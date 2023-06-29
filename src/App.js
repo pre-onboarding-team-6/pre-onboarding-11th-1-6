8ad18be00f602dae6f76347211a40d53f76ca49f
5kdk <5kdk.code@gmail.com>
2023-06-29 14:51:39 +0900

    feat: App 라우팅 구현
    Co-authored-by: kyusung <operat04@users.noreply.github.com>
    Co-authored-by: Moon il Yun <iym1511@users.noreply.github.com>
    Co-authored-by: 오현재 <hangooksaram@users.noreply.github.com>
    Co-authored-by: YUNHUI JO <YUNH7@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
-const App = () => <div></div>;
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { SignIn, SignUp, Todo } from './pages';
+import GlobalStyle from './styles/GlobalStyle';
+import AuthenticationGuard from './guards/AuthenticationGuard';
+
+const routerConfig = createBrowserRouter([
+  {
+    path: '/',
+    element: <AuthenticationGuard redirectTo={'/todo'} element={<SignIn />} />,
+  },
+  {
+    path: 'signup',
+    element: <AuthenticationGuard redirectTo={'/todo'} element={<SignUp />} />,
+  },
+  {
+    path: 'signin',
+    element: <AuthenticationGuard redirectTo={'/todo'} element={<SignIn />} />,
+  },
+  {
+    path: 'todo',
+    element: <AuthenticationGuard redirectTo={'/signin'} element={<Todo />} />,
+  },
+]);
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <RouterProvider router={routerConfig} />
+  </>
+);
 
 export default App;
