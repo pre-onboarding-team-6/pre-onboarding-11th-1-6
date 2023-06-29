1f4f008e213fa8e9ad9e4e5c1f78c3743da8c50d
5kdk <5kdk.code@gmail.com>
2023-06-29 14:49:07 +0900

    feat: 글로벌 스타일 생성
    Co-authored-by: kyusung <operat04@users.noreply.github.com>
    Co-authored-by: Moon il Yun <iym1511@users.noreply.github.com>
    Co-authored-by: 오현재 <hangooksaram@users.noreply.github.com>
    Co-authored-by: YUNHUI JO <YUNH7@users.noreply.github.com>

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    line-height: 1.4em;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  ul, li {
+    list-style: none;
+  }
+
+  button {
+    border: none;
+    text-align: center;
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyle;
